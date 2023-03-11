646978e82aba4c32ce1fa939eb77b1e1b6a64647
lucky akbar <luckyakbar1509@gmail.com>
2023-03-11 10:20:09 +0700

    fix add ampersand

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ sudo apt-get update && sudo apt-get install curl gnupg -y &&
 sudo curl --location --silent https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - &&
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' &&
 sudo apt-get update   && sudo apt-get install google-chrome-stable -y --no-install-recommends &&
-sudo rm -rf /var/lib/apt/lists/*
+sudo rm -rf /var/lib/apt/lists/* &&
 
 nvm install 16.17.0 &&
 nvm use 16.17.0 &&
